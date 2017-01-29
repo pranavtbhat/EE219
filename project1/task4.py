290a2ac8561748e149d51a771708c1d5b889c077
Isha Verma <vermaisha.21@gmail.com>
2017-01-29 12:57:02 -0800

    Check for other degrees

--- project1/task4.py
+++ project1/task4.py
@@ -43,7 +43,7 @@ for train_index, test_index in kf.split(X):
         best_y_predicted = y_predicted
         residual = y_predicted - y_test
         
-print('Best RMSE obtained was: ', best_rmse)
+print('Best RMSE obtained from linear regression was: ', best_rmse)
 print('Average RMSE obtained from linear regression was: ', rmse_sum/splits)
 
 plt.figure()
@@ -63,39 +63,49 @@ plt.show()
 #Polynomial Regression Model
 ###
 
-poly = PolynomialFeatures(degree=2)
 rmse_sum = 0
 best_rmse = float('inf')
+rmse_degree = []
 
-for train_index, test_index in kf.split(X):
-    X_train, y_train = X[train_index], y[train_index]
-    X_test, y_test = X[test_index], y[test_index]    
-    Xtrain_ = poly.fit_transform(X_train)
-    Xtest_ = poly.fit_transform(X_test)
+for deg in range(1,4):
+    poly = PolynomialFeatures(degree=deg)
+    rmse_sum = 0
+    best_rmse = float('inf')
+    for train_index, test_index in kf.split(X):
+        X_train, y_train = X[train_index], y[train_index]
+        X_test, y_test = X[test_index], y[test_index]    
+        Xtrain_ = poly.fit_transform(X_train)
+        Xtest_ = poly.fit_transform(X_test)
+    
+        reg.fit(Xtrain_, y_train)
+        y_poly_predicted = reg.predict(Xtest_)    
+        rmse = utils.rmse(y_poly_predicted, y_test)
+        rmse_sum = rmse_sum + rmse
+    
+        if rmse < best_rmse:
+            best_rmse = rmse
+            best_y = y_test
+            best_poly_y_predicted = y_poly_predicted
+            residual = y_poly_predicted - y_test
+    
+    
+    print('Best RMSE obtained from polynomial regression with degree:', deg,  'was: ', best_rmse)
+    print('Average RMSE obtained from polynomial regression with degree: ', deg, 'was: ', rmse_sum/splits)
 
-    reg.fit(Xtrain_, y_train)
-    y_poly_predicted = reg.predict(Xtest_)    
-    rmse = utils.rmse(y_poly_predicted, y_test)
-    rmse_sum = rmse_sum + rmse
+    plt.figure()
+    plt.scatter(range(len(best_y)), best_y, color='black')
+    plt.plot(range(len(best_poly_y_predicted)), best_poly_y_predicted, color='blue', linewidth = 2)
+    
+    plt.show()
+    
+    plt.figure()
+    plt.scatter(range(len(residual)), residual, color='black')
+    plt.plot(range(len(best_poly_y_predicted)), best_poly_y_predicted, color='blue', linewidth = 2)
+    
+    plt.show()     
+    rmse_degree.append(best_rmse)
 
-    if rmse < best_rmse:
-        best_rmse = rmse
-        best_coef = reg.coef_
-        best_y = y_test
-        best_poly_y_predicted = y_poly_predicted
-        residual = y_poly_predicted - y_test
-        
-print('Best RMSE obtained was: ', best_rmse)
-print('Average RMSE obtained from polynomial regression was: ', rmse_sum/splits)
 
+print('All rmse are: ', rmse_degree)
 plt.figure()
-plt.scatter(range(len(best_y)), best_y, color='black')
-plt.plot(range(len(best_poly_y_predicted)), best_poly_y_predicted, color='blue', linewidth = 2)
-
-plt.show()
-
-plt.figure()
-plt.scatter(range(len(residual)), residual, color='black')
-plt.plot(range(len(best_poly_y_predicted)), best_poly_y_predicted, color='blue', linewidth = 2)
-
-plt.show()     
+plt.plot(range(len(rmse_degree)), rmse_degree,color='blue', linewidth=3)
