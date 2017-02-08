d1e3f752c933a4be180e1a30e7ca04b8779ee7dc
Pranav <pranavtbhat@gmail.com>
2017-02-07 18:43:53 -0800

    Project 2 Part a

---
+++ project2/a.py
@@ -0,0 +1,39 @@
+from sklearn.datasets import fetch_20newsgroups
+import matplotlib.pyplot as plt
+from collections import Counter
+
+comp_tech = [
+    "comp.graphics",
+    "comp.os.ms-windows.misc",
+    "comp.sys.ibm.pc.hardware",
+    "comp.sys.mac.hardware"
+]
+
+rec_act = [
+    "rec.autos",
+    "rec.motorcycles",
+    "rec.sport.baseball",
+    "rec.sport.hockey"
+]
+
+train = fetch_20newsgroups(
+    subset = 'train',
+    categories = comp_tech + rec_act,
+    shuffle=True,
+    random_state=23
+)
+topic_names = train.target_names
+
+cc = Counter(train.target)
+freqs = [cc[i] for i in cc]
+
+plt.bar(range(1, 9), freqs, 1/1.5, color='blue')
+plt.xticks(range(1, 9), train.target_names, rotation=90)
+
+plt.subplots_adjust(bottom=0.45)
+plt.xlabel("Categories")
+plt.ylabel("Frequencies")
+
+plt.show()
+
+plt.savefig('plots/histogram.png', format='png')
