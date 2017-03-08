0efbc7114b0ed230db2e65284f143d901ab0922e
Heenal Doshi <heenald@Heenals-MacBook-Pro.local>
2017-03-08 12:02:14 -0800

    Refactoring part2

--- project4/part2.py
+++ project4/part2.py
@@ -9,8 +9,14 @@ import numpy as np
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
 
-def print_statistics(actual, predicted):
+def print_confusion_matrix(actual, predicted):
     print "Confusion Matrix is ", metrics.confusion_matrix(actual, predicted)
+
+def print_scores(actual_labels, predicted_labels):
+    print("Homogeneity: %0.3f" % metrics.homogeneity_score(actual_labels, predicted_labels))
+    print("Completeness: %0.3f" % metrics.completeness_score(actual_labels, predicted_labels))
+    print("V-measure: %0.3f" % metrics.v_measure_score(actual_labels, predicted_labels))
+    print("Adjusted Rand-Index: %.3f" % metrics.adjusted_rand_score(actual_labels, predicted_labels))
 
 if __name__ == "__main__":
 
@@ -21,10 +27,8 @@ if __name__ == "__main__":
     labels = data.target//4 #Since we want to cluster to 2 classes, and the input has 8 classes (0-7)
     kmeans = KMeans(n_clusters=2).fit(data_idf)
 
-    print_statistics(labels, kmeans.labels_)
+    print_confusion_matrix(labels, kmeans.labels_)
+    print_scores(labels, kmeans.labels_)
 
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, kmeans.labels_))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels, kmeans.labels_))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels, kmeans.labels_))
-    print("Adjusted Rand-Index: %.3f" % metrics.adjusted_rand_score(labels, kmeans.labels_))
+
 
