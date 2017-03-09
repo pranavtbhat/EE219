7f5151a085e733ada6e4bf878b65dcd797122800
Pranav <pranavtbhat@gmail.com>
2017-03-08 19:41:02 -0800

    part5

--- project4/part5.py
+++ project4/part5.py
@@ -1,24 +1,17 @@
 import part1
-import part2
 import pickle
 import os
 from sklearn import metrics
 from sklearn.datasets import fetch_20newsgroups
 from scipy.sparse import linalg as la
-from sklearn.feature_extraction import text
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
 import matplotlib.pyplot as plt
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
-from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import TruncatedSVD, NMF
 from sklearn.cluster import KMeans
 
-
-
-def get_data_idf():
-
+def get_data_idf(data):
     if os.path.exists('pkl/all_data_idf.pkl'):
         pkl_file = open('pkl/all_data_idf.pkl', 'rb')
         data_idf = pickle.load(pkl_file)
@@ -32,12 +25,10 @@ def get_data_idf():
         ]
     )
 
-    all = fetch_20newsgroups(subset='all', shuffle=True, random_state=42)
-
     print("%d documents" % len(all.filenames))
     print("%d categories" % len(all.target_names))
 
-    data_idf = pipeline.fit_transform(all.data)
+    data_idf = pipeline.fit_transform(data.data)
     print "Number of terms in TF-IDF representation:", data_idf.shape[1]
 
     output = open('pkl/all_data_idf.pkl', 'wb')
@@ -48,9 +39,7 @@ def get_data_idf():
 
 
 def find_singular_values(X):
-
     print "Finding singular values using svds.."
-
     if os.path.exists('pkl/all_singular_values_2000.pkl'):
         pkl_file = open('pkl/all_singular_values_2000.pkl', 'rb')
         s = pickle.load(pkl_file)
@@ -65,82 +54,147 @@ def find_singular_values(X):
 
     return s
 
-def find_truncated_svd_variance(X):
-
-    print "Finding Truncated SVD variance.."
-
-    if os.path.exists('pkl/all_truncated_variance.pkl'):
-        pkl_file = open('pkl/all_truncated_variance.pkl', 'rb')
-        var = pickle.load(pkl_file)
-        pkl_file.close()
-        return var
-
-    svd = TruncatedSVD(n_components=2000)
-    normalizer = Normalizer(copy=False)
-    lsa = make_pipeline(svd, normalizer)
-
-    X_lsa = lsa.fit_transform(X)
-    var = svd.explained_variance_ratio_.cumsum()
-
-    output = open('pkl/all_truncated_variance.pkl', 'wb')
-    pickle.dump(var, output)
-    output.close()
-
-    plt.plot(var)
-    plt.savefig("plots/all_truncated_variance.png", format='png')
-    plt.clf()
-
-    return var
-
-
-def save_scores(actual_labels, predicted_labels, h, c , v ,r ):
-    h.append(metrics.homogeneity_score(actual_labels, predicted_labels))
-    c.append( metrics.completeness_score(actual_labels, predicted_labels))
-    v.append( metrics.v_measure_score(actual_labels, predicted_labels))
-    r.append( metrics.adjusted_rand_score(actual_labels, predicted_labels))
-
-
+def get_scores(actual_labels, predicted_labels):
+    t0 = metrics.homogeneity_score(actual_labels, predicted_labels)
+    t1 = metrics.completeness_score(actual_labels, predicted_labels)
+    t2 = metrics.adjusted_rand_score(actual_labels, predicted_labels)
+    t3 = metrics.adjusted_mutual_info_score(actual_labels, predicted_labels)
+    return (t0, t1, t2, t3)
 
 if __name__ == "__main__":
+    data = fetch_20newsgroups(
+        subset='all',
+        shuffle=True,
+        random_state=42
+    )
+    labels = data.target
+    metric_names = [
+        'homogeneity_score',
+        'completeness_score',
+        'adjusted_rand_score',
+        'adjusted_mutual_info_score'
+    ]
 
-    data_idf = get_data_idf()
+    data_idf = get_data_idf(data)
 
-    # Uncomment below to find reduced dim
-    # s = find_singular_values(get_data_idf())
-    # var = find_truncated_svd_variance(get_data_idf())
+    ###
+    # First fix k
+    ###
+    k = 20
+    svd_metrics = []
+    nmf_metrics = []
 
-    #Selected appropriate reduced dimension using singular values
-    reduced_dim = 50
+    print "Varying Dimensions"
+    ds = range(2, 75)
+    for d in ds:
+        print "Set d = ", d
+        # Prepare SVD pipeline
+        svd = TruncatedSVD(n_components = d)
+        normalizer = Normalizer(copy=False)
+        svd_pipeline = make_pipeline(svd, normalizer)
 
-    svd = TruncatedSVD(n_components=reduced_dim)
-    normalizer = Normalizer(copy=False)
-    pipeline = make_pipeline(svd, normalizer)
-    data_lsa = pipeline.fit_transform(data_idf)
+        # Fit data and get metrics for SVD
+        X_SVD = svd_pipeline.fit_transform(data_idf)
+        kmeans_svd = KMeans(n_clusters=k).fit(X_SVD)
+        svd_metrics.append(get_scores(labels, kmeans_svd.labels_))
 
-    print "Performing Kmeans clustering.."
-    #Select appropriate k - one which gives best scores. And convert labels accordingly
+        # Prepare NMF pipeline
+        nmf = NMF(n_components = d)
+        normalizer = Normalizer(copy=False)
+        nmf_pipeline = make_pipeline(nmf, normalizer)
 
-    h=[]
-    c=[]
-    v=[]
-    r=[]
-    ks=[]
+        # Fit data and get metrics for NMF
+        X_NMF = nmf_pipeline.fit_transform(data_idf)
+        kmeans_nmf = KMeans(n_clusters=k).fit(X_NMF)
+        nmf_metrics.append(get_scores(labels, kmeans_nmf.labels_))
 
-    for k in range(6,100,1):
-
-        print "#clusters = ", k
-        kmeans = KMeans(n_clusters=k).fit(data_lsa)
-
-        labels = fetch_20newsgroups(subset='all', shuffle=True, random_state=42).target
-
-        save_scores(labels, kmeans.labels_, h,c,v,r);
-        ks.append(k)
-
-    plt.plot(ks, h, label='homogenity')
-    plt.plot(ks, c, label='completeness')
-    plt.plot(ks, v, label='normalized mutual info')
-    plt.plot(ks, r, label='rand score')
-    plt.grid()
-    plt.savefig("plots/part5.png", format='png')
+    # Plot SVD metrics
+    for i, metric_name in enumerate(metric_names):
+        plt.plot(
+            ds,
+            map(lambda x : x[i], svd_metrics),
+            label = metric_name
+        )
+    plt.xlabel('Number of Dimensions')
+    plt.ylabel('Metric value')
+    plt.legend(loc='best')
+    plt.savefig('plots/part5_fixk_svd_metrics.png', format='png')
     plt.clf()
 
+    # Plot NMF metrics
+    for i, metric_name in enumerate(metric_names):
+        plt.plot(
+            ds,
+            map(lambda x : x[i], nmf_metrics),
+            label = metric_name
+        )
+    plt.xlabel('Number of Dimensions')
+    plt.ylabel('Metric value')
+    plt.legend(loc='best')
+    plt.savefig('plots/part5_fixk_nmf_metrics.png', format='png')
+    plt.clf()
+
+    ###
+    # Next fix d
+    ###
+    # Find the best dimension size to represent the TF-IDF matrix
+    s = find_singular_values(data_idf)
+    plt.plot(sorted(s, reverse=True))
+    plt.savefig('plots/part5_singular_values.png', format='png')
+    plt.clf()
+
+    # The optimumum dimension is obtained by observing the "knee"
+    # in the plot of singular values
+    reduced_dim = 50
+    svd_metrics = []
+    nmf_metrics = []
+
+    print "Varying k"
+    ks = range(4, 31, 1)
+    for k in ks:
+        print "Set k = ", k
+        # Prepare SVD pipeline
+        svd = TruncatedSVD(n_components = reduced_dim )
+        normalizer = Normalizer(copy=False)
+        svd_pipeline = make_pipeline(svd, normalizer)
+
+        # Fit data and get metrics for SVD
+        X_SVD = svd_pipeline.fit_transform(data_idf)
+        kmeans_svd = KMeans(n_clusters=k).fit(X_SVD)
+        svd_metrics.append(get_scores(labels, kmeans_svd.labels_))
+
+        # Prepare NMF pipeline
+        nmf = NMF(n_components = reduced_dim)
+        normalizer = Normalizer(copy=False)
+        nmf_pipeline = make_pipeline(nmf, normalizer)
+
+        # Fit data and get metrics for NMF
+        X_NMF = nmf_pipeline.fit_transform(data_idf)
+        kmeans_nmf = KMeans(n_clusters=k).fit(X_NMF)
+        nmf_metrics.append(get_scores(labels, kmeans_nmf.labels_))
+
+    # Plot SVD metrics
+    for i, metric_name in enumerate(metric_names):
+        plt.plot(
+            ks,
+            map(lambda x : x[i], svd_metrics),
+            label = metric_name
+        )
+    plt.legend(loc='best')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Metric value')
+    plt.savefig('plots/part5_fixd_svd_metrics.png', format='png')
+    plt.clf()
+
+    # Plot NMF metrics
+    for i, metric_name in enumerate(metric_names):
+        plt.plot(
+            ks,
+            map(lambda x : x[i], nmf_metrics),
+            label = metric_name
+        )
+    plt.legend(loc='best')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Metric value')
+    plt.savefig('plots/part5_nmf_metrics.png', format='png')
+    plt.clf()
