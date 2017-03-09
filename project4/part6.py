afe40ac69685bf161939d570b0002a403e852483
Pranav <pranavtbhat@gmail.com>
2017-03-08 20:45:32 -0800

    Stuff

--- project4/part6.py
+++ project4/part6.py
@@ -5,29 +5,100 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import FunctionTransformer
+import matplotlib.pyplot as plt
+import numpy as np
+
+computer_technologies = [
+    'comp.graphics',
+    'comp.os.ms-windows.misc',
+    'comp.sys.ibm.pc.hardware',
+    'comp.sys.mac.hardware',
+    'comp.windows.x'
+]
+
+recreational_activity = [
+    'rec.autos',
+    'rec.motorcycles',
+    'rec.sport.baseball',
+    'rec.sport.hockey'
+]
+
+science = [
+    'sci.crypt',
+    'sci.electronics',
+    'sci.med',
+    'sci.space'
+]
+
+miscellaneus = [ 'misc.forsale' ]
+
+politics = [
+    'talk.politics.misc',
+    'talk.politics.guns',
+    'talk.politics.mideast'
+]
+
+religion = [
+    'talk.religion.misc',
+    'alt.atheism',
+    'soc.religion.christian'
+]
 
 if __name__ == "__main__":
+    # Relabelling and stuff
+    classes = [computer_technologies, recreational_activity, science, miscellaneus, politics, religion]
+    all_categories = []
+    i = 0
+    rmap = {}
+    for cnum,c in enumerate(classes):
+        for category in c:
+            all_categories.append(category)
+            rmap[i] = cnum
+            i += 1
 
-    data_idf = part5.get_data_idf()
+    data = fetch_20newsgroups(
+        subset = 'all',
+        shuffle = True,
+        random_state = 42
+    )
+    data.target = map(lambda x : rmap[x], data.target)
+    data_idf = part5.get_data_idf(data)
 
-    # s = part5.find_singular_values(data_idf)
-    # var = part5.find_truncated_svd_variance(data_idf)
-
-    # Selected appropriate reduced dimension using singular values
-    reduced_dim = 20
-
-    svd = TruncatedSVD(n_components=reduced_dim)
-    normalizer = Normalizer(copy=False)
-    pipeline = make_pipeline(svd, normalizer)
-    data_lsa = pipeline.fit_transform(data_idf)
-
-    print "Performing Kmeans clustering.."
-    # Select appropriate k - one which gives best scores. And convert labels accordingly!!!
-
+    # Find Effective dimensions to retrieve data
     k = 6
-    kmeans = KMeans(n_clusters=k).fit(data_lsa)
+    ds = range(2, 75, 1)
+    svd_metrics = []
+    print "Varying Dimensions"
+    for d in ds:
+        print "Set d = ", d
+        svd = TruncatedSVD(n_components = d)
+        poly = FunctionTransformer(np.log1p)
+        normalizer = Normalizer(copy=False)
+        svd_pipeline = make_pipeline(svd, poly, normalizer)
+        X_SVD = svd_pipeline.fit_transform(data_idf)
+        kmeans = KMeans(n_clusters = k).fit(X_SVD)
+        svd_metrics.append(part5.get_scores(data.target, kmeans.labels_))
 
-    labels = fetch_20newsgroups(subset='all', shuffle=True, random_state=42).target
+        part2.print_confusion_matrix(data.target, kmeans.labels_)
+        part2.print_scores(data.target, kmeans.labels_)
 
-    part2.print_confusion_matrix(labels, kmeans.labels_)
-    part2.print_scores(labels, kmeans.labels_)
+    metric_names = [
+        'homogeneity_score',
+        'completeness_score',
+        'adjusted_rand_score',
+        'adjusted_mutual_info_score'
+    ]
+    for i, metric_name in enumerate(metric_names):
+        plt.plot(
+            ds,
+            map(lambda x : x[i], svd_metrics),
+            label = metric_name
+        )
+    plt.legend(loc='best')
+    plt.xlabel('Number of Dimensions')
+    plt.ylabel('Metric value')
+    plt.savefig('plots/part6.png', format='png')
+    plt.clf()
+
+
