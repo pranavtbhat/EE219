02a32c5815711d35b0ae46e450ffcec895e10072
Pranav <pranavtbhat@gmail.com>
2017-03-17 19:01:59 -0700

    More stuff

--- project5/part1.py
+++ project5/part1.py
@@ -1,5 +1,4 @@
 import json
-import datetime, time
 import numpy as np
 from os.path import join
 from tqdm import tqdm
@@ -23,62 +22,44 @@ print "Computing Statistics for hashtags:"
 for (htag,lcount) in hashtags.iteritems():
     print "#", htag + ":"
 
-    tweet_list = []
-    timestamps = []
-    num_tweets = 0
-
     with open(join('tweet_data', 'tweets_#' + htag + '.txt'), 'r') as f:
+        cw = 1
+        users = {}
+
+        # Fetch first tweet
+        first_tweet = json.loads(f.readline())
+        f.seek(0, 0)
+
+        num_tweets_in_hour = []
+        number_of_retweets = 0
+
+        start_time = first_tweet.get('firstpost_date')
+        end_of_window = start_time + cw * 3600
+        current_hour_count = 0
+
         for line in tqdm(f, total=lcount):
-            tweet = json.loads(line)
-            tweet_list.append(tweet)
-            timestamps.append(tweet['firstpost_date'])
+            tweet_data = json.loads(line)
+            tweet_time = tweet_data.get('firstpost_date')
 
-        print "Loaded ", num_tweets, "tweets"
+            if tweet_time < end_of_window:
+                current_hour_count += 1
+            else:
+                number_of_retweets += tweet_data.get('metrics').get('citations').get('total')
+                num_tweets_in_hour.append(current_hour_count)
+                cw += 1
+                current_hour_count = 0
+                end_of_window = start_time + cw * 3600
 
-        start_date = datetime.datetime(2015, 01, 30, 0, 0, 0)
-        end_date = datetime.datetime(2015, 02, 03, 0, 0, 0)
-        mintime = int(time.mktime(start_date.timetuple()))
-        maxtime = int(time.mktime(end_date.timetuple()))
-        date_reference = datetime.datetime(2015, 01, 01, 0, 0, 0)
-        time_reference = int(time.mktime(date_reference.timetuple()))
+            user = tweet_data.get('tweet').get('user').get('id')
+            users[user] = tweet_data.get('author').get('followers')
 
-        print "extracting features"
-
-        num_hours = int((maxtime-mintime)/3600)
-        num_tweets_in_hour = [0] * num_hours
-        num_retweets_in_hour = [0] * num_hours
-        sum_followers = [0] * num_hours
-        max_followers = [0] * num_hours
-
-        time_of_day = [0] * num_hours
-        mintime_daytime = int((mintime-time_reference)/3600) % 24
-
-        for t in range(0,num_hours):
-            time_of_day[t] = mintime_daytime + t
-
-        for i in range(0,num_tweets):
-            tweet = tweet_list[i]
-            tweet_time = tweet['firstpost_date']
-            if tweet_time >= mintime:
-                hour = int((tweet_time-mintime)/3600)
-                if hour >= num_hours:
-                    break;
-                num_tweets_in_hour[hour] += 1
-                try:
-                    num_retweets_in_hour += tweet['metrics']['citations']['data'][0]['citations']
-                except:
-                    pass
-                sum_followers[hour] += tweet['tweet']['user']['followers_count']
-                max_followers[hour] = max(max_followers[hour], tweet['tweet']['user']['followers_count'])
-
-
-        print "Average number of tweets per hour", np.mean(num_tweets_in_hour)
-        print "Average number of followers of authors", np.mean(sum_followers)
-        print "Average number of retweets", np.mean(num_retweets_in_hour)
+        print "Average number of tweets per hour", lcount / ((tweet_time - start_time) / 3600.0)
+        print "Average number of followers of authors", np.mean(users.values())
+        print "Average number of retweets", number_of_retweets / lcount
 
         if htag in ['superbowl', "nfl"]:
             plt.ylabel('Number of Tweets')
             plt.xlabel('Hour')
             plt.title('Number of Tweets per hour for {}'.format(htag))
-            plt.bar(range(len(num_tweets_in_hour)), num_retweets_in_hour)
+            plt.bar(range(len(num_tweets_in_hour)), num_tweets_in_hour)
             plt.savefig(htag + '_statistics.png', format='png')
